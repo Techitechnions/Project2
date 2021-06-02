93892eac1e48c30a257a03f245acc4f9900af5b2
abhavya-tech <verma.abhavya@gmail.com>
2021-06-02 13:50:49 +0530

    Insertion Query added

--- database.js
+++ database.js
@@ -7,16 +7,33 @@
 const {Pool,Client}= require('pg')
 
 
-//const connectionString='postgressql://username:password@localhost:5432/databasename'
+const connectionString='postgressql://username:password@localhost:5432/databasename'
 
 
 //Creating Client
 const client= new Client({
     connectionString:connectionString
 })
+//Select query
+// client.connect()
+// client.query('Select * from public."Stud"',(err,res)=> {
+//     console.log(err,res)
+//     //client.end()
+// })
 
+
+// Select Query. Check the structure of your table as shown in the video
+//client.connect()
+// client.query('Select * from Form',(err,res)=> {
+//     console.log(err,res)
+//     //client.end()
+// })
+
+
+// Insertion Query. Check the structure of your table as shown in the video
 client.connect()
-client.query('Select * from public."Stud"',(err,res)=> {
+const qr="Insert into Form Values('Name','mail',6712)";
+client.query(qr,(err,res)=> {
     console.log(err,res)
     client.end()
 })
